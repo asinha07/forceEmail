55b1bdcb9daa61c06577347050cacb3d817f0735
Aman Sinha <aman.sinha@quikr.com>
2018-04-24 15:36:54 +0530

    Final commit

--- autoReadMail.py
+++ autoReadMail.py
@@ -23,30 +23,34 @@ def sendemail(from_addr, to_addr_list,
     problems = server.sendmail(from_addr, to_addr_list, message)
     server.quit()
 
-try:
-    mail = imaplib.IMAP4_SSL(SMTP_SERVER)
-    mail.login(FROM_EMAIL,FROM_PWD)
-    mail.select('inbox')
+def readmail():
+    try:
+        mail = imaplib.IMAP4_SSL(SMTP_SERVER)
+        mail.login(FROM_EMAIL,FROM_PWD)
+        mail.select('inbox')
 
-    type, data = mail.search(None, 'ALL')
-    mail_ids = data[0]
-    id_list = mail_ids.split()
-    first_email_id = int(id_list[0])
-    latest_email_id = int(id_list[-1])
-    for i in range(latest_email_id,first_email_id, -1):
-        typ, data = mail.fetch(i, '(RFC822)' )
+        type, data = mail.search(None, 'ALL')
+        mail_ids = data[0]
+        id_list = mail_ids.split()
+        first_email_id = int(id_list[0])
+        latest_email_id = int(id_list[-1])
+        for i in range(latest_email_id,first_email_id, -1):
+            typ, data = mail.fetch(i, '(RFC822)' )
 
-        for response_part in data:
-            if isinstance(response_part, tuple):
-                msg = email.message_from_string(response_part[1])
-                email_subject = msg['subject']
-                email_from = msg['from']
-                date = msg['date']
-                if email_from == 'THE FUCKER"S EMAIL':
-                    sendemail(FROM_EMAIL,[to_email_list],email_subject,msg,FROM_EMAIL,FROM_PWD);
-                    print 'From : ' + email_from + '\n'
-                    print 'Subject : ' + email_subject + '\n'
-                    print 'Date: '+date
+            for response_part in data:
+                if isinstance(response_part, tuple):
+                    msg = email.message_from_string(response_part[1])
+                    email_subject = msg['subject']
+                    email_from = msg['from']
+                    date = msg['date']
+                    if email_from == 'THE FUCKER"S EMAIL':
+                        sendemail(FROM_EMAIL,[to_email_list],email_subject,msg,FROM_EMAIL,FROM_PWD);
+                        print 'From : ' + email_from + '\n'
+                        print 'Subject : ' + email_subject + '\n'
+                        print 'Date: '+date
 
-except Exception, e:
-    print str(e)
+    except Exception, e:
+        print str(e)
+
+if __name__ == 'main':
+    readmail()
